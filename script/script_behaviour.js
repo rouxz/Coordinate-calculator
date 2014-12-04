70809f9b508f5f996fbcf0b61138c345508fd859
rouxz <rouxz@ymail.com>
2014-12-04 10:54:12 +0100

    4DEC 1054

--- script/script_behaviour.js
+++ script/script_behaviour.js
@@ -1,9 +1,43 @@
 'use strict';
+var COORDINATE_FINDER = COORDINATE_FINDER || {};
 
 
-function retrieveCoordinate() {
-    map_routines.coordinateFinder('Pau');
-    alert('Retrieve');
-}
     
     
+	
+/* include map in the web page */
+google.maps.event.addDomListener(window, 'load', MAP_ROUTINES.initialize);
+
+
+/* for implementation */
+/* ------------------ */
+COORDINATE_FINDER.retrieveCoordinate = function () {
+    MAP_ROUTINES.coordinateFinder();
+};
+
+
+/* load when the DOM is ready */
+/*
+$().ready(function () {
+
+    
+
+	
+//		$.getJSON("data/data_feed_vba.json")
+//			/* load the map and fill data and markers in it */
+//			.done(function(data){
+//					
+//					/* add all the markers */
+//					addMarkers(data);
+//					/* add a behaviour to the form */
+//					add_form_behaviour();
+//					
+//			})
+//			.fail(function(){
+//					/* show that the json loading has failed */
+//					$("#messages").html("results loading failed");
+//		});
+
+	//end of ready	
+//});
+
--- script/script_gmap.js
+++ script/script_gmap.js
@@ -1,81 +1,55 @@
-/* clear space of name */
-(function map_routines () {
-    'use strict';
-	var map;
-	var centerMap = new google.maps.LatLng(48.806863, 17.050781);
-	var marker;
+/* routines for handling google maps */
+var MAP_ROUTINES = {};
 
-    
-    
-	// create a map
-	function initialize() {
-		var mapOptions = {
-			zoom: 4,
-			center: centerMap,
-			mapTypeId: google.maps.MapTypeId.TERRAIN,
-			mapTypeControl: false,
-			streetViewControl: false,
-			maxZoom: 16,
-			minZoom: 0
-		};
-		// create the map
-		map = new google.maps.Map(document.getElementById('map'), mapOptions);
-	}
+//var map;
+MAP_ROUTINES.centerMap = new google.maps.LatLng(48.806863, 17.050781);
+//var marker;
 
-    // find out coordinates in a map
-    function coordinateFinder(value) {
-        // call google map geocode service
-        geocoder.geocode({ 'address': value}, function (results, status) {
-            // result found
-            if (status == google.maps.GeocoderStatus.OK) {
-                map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: results[0].geometry.location
-                });
-                alert("Found");
-                return results[0].geometry.location;
-            }  
+/* create a map */
+MAP_ROUTINES.initialize = function () {
+    var mapOptions = {
+        zoom: 4,
+        center: MAP_ROUTINES.centerMap,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        mapTypeControl: false,
+        streetViewControl: false,
+        maxZoom: 16,
+        minZoom: 0
+    };
+    // create the map
+    MAP_ROUTINES.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+/* find out coordinates in a map */
+MAP_ROUTINES.coordinateFinder = function () {
+    // call google map geocode service
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': 'NYC'}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            /*// result found
+            MAP_ROUTINES.map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: MAP_ROUTINES.map,
+                position: results[0].geometry.location
+            });*/
+            alert('Found');
+            //return results[0].geometry.location;
+        } else {
             //result no found
-            else {
-                alert('not found');
-                return 'Geocode was not successful for the following reason: ' + status;
-            }
-        });
-
-    }
-    
-    
-    
-    
-    
-    
-    
-	
-	/* include map in the web page */
-	google.maps.event.addDomListener(window, 'load', initialize);
-				
-	
-	/* load when the DOM is ready */
-	$().ready(function () {
-
-
-		
-//		$.getJSON("data/data_feed_vba.json")
-//			/* load the map and fill data and markers in it */
-//			.done(function(data){
-//					
-//					/* add all the markers */
-//					addMarkers(data);
-//					/* add a behaviour to the form */
-//					add_form_behaviour();
-//					
-//			})
-//			.fail(function(){
-//					/* show that the json loading has failed */
-//					$("#messages").html("results loading failed");
-//		});
-
-	//end of ready	
+            alert('not found');
+            //return 'Geocode was not successful for the following reason: ' + status;
+        }
     });
-}());
+
+}
+
+    
+    
+    
+    
+    
+    
+
+
+
+
