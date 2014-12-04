08d06fd3806fce1ba30d9987c18c14ddeb42de7a
rouxz <rouxz@ymail.com>
2014-12-04 16:34:24 +0100

    04DEC14 1634

--- script/script_gmap.js
+++ script/script_gmap.js
@@ -1,9 +1,12 @@
 /* routines for handling google maps */
 var MAP_ROUTINES = {};
 
-//var map;
+/* all the markers on the map */
+MAP_ROUTINES.markers = [];
+
+
 MAP_ROUTINES.centerMap = new google.maps.LatLng(48.806863, 17.050781);
-//var marker;
+
 
 /* create a map */
 MAP_ROUTINES.initialize = function () {
@@ -22,13 +25,27 @@ MAP_ROUTINES.initialize = function () {
 
 /*clear all the markers on a map */
 MAP_ROUTINES.clearMarkers = function (){
-    
+    var i;
+    /* clear markers from the map */
+    for (i = 0; i < MAP_ROUTINES.markers.length; i++){
+        MAP_ROUTINES.markers[i].setMap(null);
+    }
+    /* clear markers */
+    MAP_ROUTINES.markers = [];
+}
+
+
+/* add a marker on the map */
+MAP_ROUTINES.addMarker = function (marker){
+    MAP_ROUTINES.markers.push(marker);
+    marker.setMap(MAP_ROUTINES.map);
 }
 
 
 
 /* find out coordinated in a map */
 /* return results via a callback function */
+/* put a market on the map */
 MAP_ROUTINES.coordinateFinder = function (city, callback) {
     // call google map geocode service
     var geocoder = new google.maps.Geocoder();
@@ -37,10 +54,10 @@ MAP_ROUTINES.coordinateFinder = function (city, callback) {
             if (status == google.maps.GeocoderStatus.OK) {
                 // result found
                 MAP_ROUTINES.map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
+                MAP_ROUTINES.addMarker(new google.maps.Marker({
                     map: MAP_ROUTINES.map,
                     position: results[0].geometry.location
-                });
+                }));
                 callback(city, results[0].geometry.location);
             } else {
                 //result no found
